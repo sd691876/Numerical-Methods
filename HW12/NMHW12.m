507559a65fb9f20272b8187f49f817fa520fe510
sd691876 <55582065+sd691876@users.noreply.github.com>
2020-04-26 19:45:38 +0800

    4.26

--- HW12/NMHW12.m
+++ HW12/NMHW12.m
@@ -9,8 +9,26 @@ v = [6.078e-11, 7.595e-9, 6.063e-8, 5.788e-6, 1.737e-5, 2.423e-5, ...
  r2 = 1 - sr/st
  
 load Y12.txt
-Z=-(1./(Y12(:,2)+1j*Y12(:,3)));
+Z=-(1./(Y12(:,2)+1j*Y12(:,3))); % impedance
 f=Y12(:,1);
-plot(f,imag(Z));
+figure;
+plot(f*1e-9,real(1./Z),'-b','lineWidth',3); hold on;
+plot(f*1e-9,imag(1./Z),'-g','lineWidth',3); hold off;
+legend('Real','Imag');
+xlabel('Freq(GHz)'); ylabel('Y');
 
-Z_img = w * L - 1 /(w * C);
+f  = f(f<=4e+9);
+y  = imag(Z(1:length(f)));
+z  =  [2*pi*f -(1./(2*pi*f))];
+a  = (z' * z)\(z' * y);
+L  = a(1);
+C  = 1/a(2);
+Eq = @(f) 2*pi*f*L - 1./(2*pi*f*C);
+St = sum((y-mean(y)).^2);
+Sr = sum((y-z*a).^2);
+r2 = 1 - Sr/St;
+figure;
+plot(f*1e-9,y,'-b','lineWidth',3); hold on;
+plot(f*1e-9,Eq(f),'-g','lineWidth',1); hold off;
+legend('Measured','Fitted');
+xlabel('Freq(GHz)'); ylabel('imag(Z)');

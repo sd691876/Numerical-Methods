9b0c95200bb6a61fb4e350c4659571ea08451bba
sd691876 <55582065+sd691876@users.noreply.github.com>
2020-05-20 14:55:32 +0800

    5.20

--- HW15/adapsimp3.m
+++ HW15/adapsimp3.m
@@ -3,13 +3,14 @@ function [v, N,n]=adapsimp3(func, a, b, tol, maxseg)
     v=Simp3(func, a, (a+b)/2)+Simp3(func, (a+b)/2, b);
 if abs((v-v0)/v)<tol || maxseg<8
     N=4;
-    n = [a (a+b)/2 b];
+    n = [ a ((a+b)/2+a)/2 (a+b)/2 ((a+b)/2+b)/2 b];
 else
     [v1, N1, n1]=adapsimp3(func, a, (a+b)/2, tol, maxseg-4);
     [v2, N2, n2]=adapsimp3(func, (a+b)/2, b, tol, maxseg-N1);
     v=v1+v2;
     N=N1+N2;
-    n = [n1,n2];
+    n = [n1,n2(2:end)];
 end
+
 
 
